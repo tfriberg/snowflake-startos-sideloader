8ffc903ca832ee0e3ffabfd2980d3a66745dbecd
Matt Hill <mattnine@protonmail.com>
2026-05-26 13:58:16 -0600

    bump start-sdk to 1.5.3 (2.0.0:7)

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,13 +1,13 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 
 export const current = VersionInfo.of({
-  version: '2.0.0:6',
+  version: '2.0.0:7',
   releaseNotes: {
-    en_US: 'Bumps start-sdk → 1.5.2.',
-    es_ES: 'Actualiza start-sdk → 1.5.2.',
-    de_DE: 'Aktualisiert start-sdk → 1.5.2.',
-    pl_PL: 'Aktualizuje start-sdk → 1.5.2.',
-    fr_FR: 'Met à jour start-sdk → 1.5.2.',
+    en_US: 'Bumps start-sdk → 1.5.3.',
+    es_ES: 'Actualiza start-sdk → 1.5.3.',
+    de_DE: 'Aktualisiert start-sdk → 1.5.3.',
+    pl_PL: 'Aktualizuje start-sdk → 1.5.3.',
+    fr_FR: 'Met à jour start-sdk → 1.5.3.',
   },
   migrations: {
     up: async ({ effects }) => {},
